aeacd8f5805c3475edb196da617653537209bffe
ramin98 <59912261+ramin98@users.noreply.github.com>
2022-05-26 16:29:26 +0400

    Update Messages.js

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,38 +1,20 @@
-import axios from "axios";
 import Pagination from "react-js-pagination";
-import {useEffect,useState} from "react";
 
-export default function Messages({show}){
-const [data,setData] = useState([])
-const [activePage,activePageSet] = useState(1)
-    
-useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts?_page=1&_limit=10")
-      .then(res => {
-        setData(res.data)        
-      });
-      console.log(data)
-  },[]) 
-  function handlePageChange(pageNumber){
-    axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${pageNumber}&_limit=10`)
-      .then(res => {
-        setData(res.data)
-      });
-      activePageSet(pageNumber)
-  };
+export default function Messages({show,active,handle,dataPosts}){
+
   
     return (
       <div className="messages">
           
         <h2>Messages</h2>
-       <ul>{data.map(item => {
+       <ul>{dataPosts.map(item => {
       return <li onClick={() => {show(true)}} key={item.id}>{item.title}</li>;
     })}</ul>
         <Pagination
-          activePage={activePage}
+          activePage={active}
           itemsCountPerPage={10}
           totalItemsCount={100}
-          onChange={handlePageChange}
+          onChange={handle}
         />
       </div>
     );
