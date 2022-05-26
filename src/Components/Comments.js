6fb910f57f8bf5998a346d8e40023d8148b1a586
ramin98 <59912261+ramin98@users.noreply.github.com>
2022-05-26 16:31:04 +0400

    Update Comments.js

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,38 +1,20 @@
-import axios from "axios";
 import Pagination from "react-js-pagination";
-import {useEffect,useState} from "react";
 
-export default function Comments({show}){
-const [data,setData] = useState([])
-const [activePage,activePageSet] = useState(1)
-    
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/comments?_page=1&_limit=5")
-      .then(res => {
-        setData(res.data)        
-      });
-      console.log(data)
-  },[]) 
-  function handlePageChange(pageNumber){
-    axios.get(`https://jsonplaceholder.typicode.com/comments?_page=${pageNumber}&_limit=5`)
-      .then(res => {
-        setData(res.data)
-      });
-      activePageSet(pageNumber)
-  };
+export default function Comments({show,active,handle,dataComments}){
   
     return (
       <div className="comments">
+        <span onClick={() => {show(false)}} className="exit">X</span>
           <h2>Comments</h2>
-          <span onClick={() => {show(false)}} className="exit">X</span>
-       <ul>{data.map(item => {
+          
+       <ul>{dataComments.map(item => {
       return <li key={item.id}>{item.body}</li>;
     })}</ul>
         <Pagination
-          activePage={activePage}
+          activePage={active}
           itemsCountPerPage={5}
           totalItemsCount={5}
-          onChange={handlePageChange}
+          onChange={handle}
         />
       </div>
     );
