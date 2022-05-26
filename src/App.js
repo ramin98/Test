b0a60d47caa3255624fcdc1141dcdadb5a740d36
ramin98 <59912261+ramin98@users.noreply.github.com>
2022-05-26 13:39:21 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Messages from "./Components/Messages";
 import Comments from "./Components/Comments";
 import Users from "./Components/Users";
 import {useState} from "react";
+import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
 
 export default function App(){
 
@@ -9,10 +10,17 @@ const [show,showSet] = useState(false)
     
     return (
       <div className="App">
-        <Users/>
-
-        <Messages show={showSet}/>
-
+        <BrowserRouter>
+        <div className="nav">
+        <Link to='/Components/Users'>Users</Link>
+        <Link to='/Components/Messages'>Messages</Link>
+        </div>
+        
+        <Routes>
+        <Route path="/Components/Messages" element={<Messages show={showSet}/>}/> 
+        <Route path="/Components/Users" element={<Users/>}/> 
+        </Routes>
+        </BrowserRouter>
         {show && <Comments show={showSet}/>} 
       </div>
     );
